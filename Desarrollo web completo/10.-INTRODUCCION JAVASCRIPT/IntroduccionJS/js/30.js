e725587a78c73ddb2f532661dfabe9b1c851dd8c
XeanN <angel.xp.pb@gmail.com>
2022-11-06 08:48:06 -0500

    nuevo

---
+++ Desarrollo web completo/10.-INTRODUCCION JAVASCRIPT/IntroduccionJS/js/30.js
@@ -0,0 +1,18 @@
+const usuarioAutenticado = new Promise( (resolve, reject) => {
+    const auth = true;
+
+    if(auth) {
+        resolve('Usuario Auténticado'); // El PROMISE se cumple
+    } else {
+        reject('No se pudo iniciar sesión'); // El PROMISE no se cumple
+    }
+})
+
+usuarioAutenticado
+    .then( resultado => console.log(resultado))
+    .catch( error => console.log(error))
+
+ // En los Promises existen 3 valores
+ // Pending: No se ha cumplido pero  tampoco se ha rechazado
+ // Fulfilled: Ya se cumplio
+ // Rejected: Se ha rechazado o no se pudo cumplir
